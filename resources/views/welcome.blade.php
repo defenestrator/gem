de2433883cfb27ef6b8c8b92499ec1a58cdfd2b3
Jeremy Anderson <29446+defenestrator@users.noreply.github.com>
2023-09-16 07:52:31 -0600

    dark

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
                     
-                    <h2 class="font-bold text-white text-lg">Gem Reptiles</h2>
+                    <h2 class="font-bold dark:text-white text-lg">Gem Reptiles</h2>
                 </div>
                 <div class="flex justify-center"><img src="favicon.png" /></div>
 
