5a1dfa47fbfebe0e3382a7e4a1cfa1688a552948
Jeremy Anderson <29446+defenestrator@users.noreply.github.com>
2023-09-17 07:18:27 -0600

    schema1.0dump

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:heartbeat')->thenPing('http://beats.envoyer.io/heartbeat/uXCYSo1Y3nJTELd')->everyMinute();
+        $schedule->command('app:heartbeat')->everyMinute()->thenPing('http://beats.envoyer.io/heartbeat/uXCYSo1Y3nJTELd');
     }
 
     /**
