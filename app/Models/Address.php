f54254bb01a3916a47bf10de6d2c8e62ca733494
Jeremy Anderson <29446+defenestrator@users.noreply.github.com>
2023-09-17 12:12:27 -0600

    model update checkpoint and adds new migrations for laravel/passport

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Dyrynda\Database\Support\GeneratesUuid;
+use Dyrynda\Database\Casts\EfficientUuid;
 
 class Address extends Model
 {
-    use HasFactory;
+    use HasFactory, GeneratesUuid;
+
+    protected $casts = [
+        'uuid' => EfficientUuid::class,
+    ];
+
+    protected $fillable = [
+        'nickname',
+        'country',
+        'street_address',
+        'unit_number',
+        'city',
+        'state',
+        'postal_code',
+        'latitude',
+        'longitude'
+    ];
+
+    public function addressable()
+    {
+        return $this->morphTo();
+    }
 }
--- app/Models/Brood.php
+++ app/Models/Brood.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Dyrynda\Database\Support\GeneratesUuid;
+use Dyrynda\Database\Casts\EfficientUuid;
 
 class Brood extends Model
 {
-    use HasFactory;
+    use HasFactory, GeneratesUuid;
+
+    protected $casts = [
+        'uuid' => EfficientUuid::class,
+    ];
+
+    public function pairing()
+    {
+        return $this->belongsToMany(Pairing::class);
+    }
 }
