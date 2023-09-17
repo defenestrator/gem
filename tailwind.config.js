d054eb4783ac1b4ef82e77e19006df922b07db8a
Jeremy Anderson <29446+defenestrator@users.noreply.github.com>
2023-09-17 09:42:58 -0600

    colors and type

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,26 @@ export default {
 
     theme: {
         extend: {
+            colors: {
+                green: {
+                    '50':  '#EEF3ED',
+                    '100': '#D4E4D0',
+                    '200': '#82AC77',
+                    '300': '#ADCBA5',
+                    '400': '#5C8B50',
+                    '500': '#3b7d2b',
+                    '600': '#3e6534',
+                    '700': '#29571e',
+                    '800': '#183f0d',
+                    '900': '#0A2602',
+                }
+            },
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+                serif: ['"Fauna One"', ...defaultTheme.fontFamily.serif]
             },
         },
-    },
 
     plugins: [forms, require("daisyui")],
-};
+    }
+}
