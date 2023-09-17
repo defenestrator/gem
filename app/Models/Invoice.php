f8bf89510dc9aa74b9669620c37ef9e27ac0d906
Jeremy Anderson <29446+defenestrator@users.noreply.github.com>
2023-09-17 13:23:53 -0600

    fuckstained_my_freezer_broke

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Dyrynda\Database\Support\GeneratesUuid;
+use Dyrynda\Database\Casts\EfficientUuid;
 
 class Invoice extends Model
 {
-    use HasFactory;
+    use HasFactory, GeneratesUuid;
+
+    protected $casts = [
+        'uuid' => EfficientUuid::class,
+    ];
 }
