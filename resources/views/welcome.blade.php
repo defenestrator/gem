a50fa8fe38d91d72821c905d6df3fcd43fbd224a
Jeremy Anderson <29446+defenestrator@users.noreply.github.com>
2023-10-31 10:30:25 -0600

    launch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,6 +42,7 @@
                 <div class="flex justify-center">
                     
                     <h2 style="color:#FF9900;">The Wild Type</h2>
+                    <h3>Coming soon, Q1 2024!</h3>
                 </div>
 
             </div>
