956de881cef2057e51f33095a050bf274d48c780
Jeremy Anderson <29446+defenestrator@users.noreply.github.com>
2025-11-13 11:34:19 -0700

    fings

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,11 @@
     <x-slot name="background">
     </x-slot> 
     <div class="w-full min-h-screen flex justify-center items-center">
-        <div class="text-left bg-white bg-opacity-85 dark:bg-gray-800 dark:text-gray-200 p-12 rounded-xl shadow-l2xl shadow-inner">
-            <h1 class="text-4xl font-bold dark:bg-gray-800 text-gray-800 dark:text-gray-200">Gem Reptiles</h1>
-            <h2 class="mt-8" >Captive-bred Pythons and Colubrids</h2>
-            <h3 class="mt-4 text-lg text-orange-600 dark:text-gray-400">The Wild Type</h3>
-            <h4 class="mt-4 text-lg text-orange-600 dark:text-gray-400">Q4 2025 Availability: </h4>
+        <div id="main-tile" class="text-left min-h-[70vh] bg-opacity-90 bg-gray-800 dark:text-gray-200 p-12 rounded-xl shadow-l2xl shadow-inner">
+            <h1 class="text-4xl font-bold">Gem Reptiles</h1>
+            <h3 class="mt-4 text-lg dark:text-gray-400">The Wild Type</h3>
+            <h2 class="mt-8 text-xl" >Captive-bred Pythons and Colubrids</h2>
+            <h4 class="mt-4 font-bold text-2xl">Q4 2025 Availability: </h4>
         </div>
     </div>
 </x-guest-layout>
