b2b7cc388e4cfe1d9cd58d1ad9a1ef5e3cfad054
Jeremy Anderson <29446+defenestrator@users.noreply.github.com>
2026-04-20 14:34:17 -0600

    design basic improvements

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,12 @@
             <h1 class="mt-4 text-3xl" style="color:#ce4f00;">The Wild Type</h1>
             <h2 class="mt-8 text-xl" >Captive-bred Pythons and Colubrids</h2>
             <h4 class="mt-4 font-bold text-2xl"><a href="https://www.morphmarket.com/stores/gem" title="Gem Reptiles on MorphMarket">Visit our MorphMarket Store!</a></h4>
-            <a href="https://www.morphmarket.com/stores/gem" title="Gem Reptiles MorphMarket Store">Gem Reptiles on MorphMarket</a>
+            <h5 class="link m-4 p-4"><a href="https://www.morphmarket.com/stores/gem" title="Gem Reptiles MorphMarket Store">Gem Reptiles on MorphMarket</a></h5>
+            <div class="mx-auto flex justify-center">
+                <a href="/available"><button class="bg-orange-500 text-white py-2 px-4 rounded-lg hover:bg-orange-700">
+                    Available Animals
+                </button></a>
+            </div>
         </div>
     </div>
 </x-guest-layout>
