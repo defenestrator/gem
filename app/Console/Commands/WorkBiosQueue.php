ba3ed0f5e25aad3ea3ebd8a825c16f0ae4fe86b5
Jeremy Anderson <29446+defenestrator@users.noreply.github.com>
2026-05-05 10:59:51 -0600

    Add species:work-bios queue worker command for species-bios queue

---
+++ app/Console/Commands/WorkBiosQueue.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class WorkBiosQueue extends Command
+{
+    protected $signature = 'species:work-bios
+        {--once    : Process a single job then exit}
+        {--tries=3 : Number of times to attempt a job before marking it failed}
+        {--timeout=120 : Seconds a job may run before it is killed}';
+
+    protected $description = 'Start a queue worker for the species-bios queue';
+
+    public function handle(): int
+    {
+        $args = [
+            'connection' => config('queue.default'),
+            '--queue'    => 'species-bios',
+            '--sleep'    => 3,
+            '--tries'    => (int) $this->option('tries'),
+            '--timeout'  => (int) $this->option('timeout'),
+        ];
+
+        if ($this->option('once')) {
+            $args['--once'] = true;
+        }
+
+        return $this->call('queue:work', $args);
+    }
+}
