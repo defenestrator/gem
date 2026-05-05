9d43280d4d747de573a0290854bc3149b4d061e8
Jeremy Anderson <29446+defenestrator@users.noreply.github.com>
2026-05-04 19:37:28 -0600

    He's going the distance, he's going for speed

--- resources/views/animals/index.blade.php
+++ resources/views/animals/index.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
             <!-- Search & Filter -->
             <div class="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-md mb-6">
