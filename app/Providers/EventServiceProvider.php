073fd811e19a744f70be8a65696425d1004b9856
Jeremy Anderson <29446+defenestrator@users.noreply.github.com>
2026-05-06 19:04:23 -0600

    Add mail send/failure logging via event listeners; retry 12 failed mailpit-era jobs

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,32 +5,50 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Mail\Events\MessageSending;
+use Illuminate\Mail\Events\MessageSent;
+use Illuminate\Queue\Events\JobFailed;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Log;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event to listener mappings for the application.
-     *
-     * @var array<class-string, array<int, class-string>>
-     */
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
     ];
 
-    /**
-     * Register any events for your application.
-     */
     public function boot(): void
     {
-        //
+        Event::listen(MessageSending::class, function (MessageSending $event) {
+            Log::info('mail.sending', [
+                'to'      => array_keys($event->message->getTo()),
+                'subject' => $event->message->getSubject(),
+                'mailer'  => config('mail.default'),
+                'host'    => config('mail.mailers.' . config('mail.default') . '.host'),
+            ]);
+        });
+
+        Event::listen(MessageSent::class, function (MessageSent $event) {
+            Log::info('mail.sent', [
+                'to'      => array_keys($event->message->getTo()),
+                'subject' => $event->message->getSubject(),
+            ]);
+        });
+
+        Event::listen(JobFailed::class, function (JobFailed $event) {
+            if (str_contains($event->job->resolveName(), 'Mail') || str_contains($event->job->resolveName(), 'Mailable')) {
+                Log::error('mail.job_failed', [
+                    'job'       => $event->job->resolveName(),
+                    'exception' => $event->exception->getMessage(),
+                    'mailer'    => config('mail.default'),
+                    'host'      => config('mail.mailers.' . config('mail.default') . '.host'),
+                ]);
+            }
+        });
     }
 
-    /**
-     * Determine if events and listeners should be automatically discovered.
-     */
     public function shouldDiscoverEvents(): bool
     {
         return false;
