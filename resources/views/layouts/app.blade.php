dd238fb37a588533ea42ba519afd76803039cfde
Jeremy Anderson <29446+defenestrator@users.noreply.github.com>
2026-05-07 07:20:52 -0600

    Serve favicons from DO Spaces assets/; update webmanifest icon URLs to absolute CDN paths

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,12 +9,12 @@
         @stack('meta')
 
         <!-- Favicons -->
-        <link rel="icon" type="image/x-icon" href="/favicon.ico">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="manifest" href="/site.webmanifest">
-        <meta name="msapplication-TileImage" content="/ms-favicon.png">
+        <link rel="icon" type="image/x-icon" href="https://gemx.sfo3.digitaloceanspaces.com/assets/favicon.ico">
+        <link rel="icon" type="image/png" sizes="32x32" href="https://gemx.sfo3.digitaloceanspaces.com/assets/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="https://gemx.sfo3.digitaloceanspaces.com/assets/favicon-16x16.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="https://gemx.sfo3.digitaloceanspaces.com/assets/apple-touch-icon.png">
+        <link rel="manifest" href="https://gemx.sfo3.digitaloceanspaces.com/assets/site.webmanifest">
+        <meta name="msapplication-TileImage" content="https://gemx.sfo3.digitaloceanspaces.com/assets/ms-favicon.png">
         <meta name="msapplication-TileColor" content="#f97316">
         <meta name="theme-color" content="#f97316">
 
